a22144db64107b5cd27b18a41683a457bcab04a9
kelvesc <kelves_16@hotmail.com>
2024-08-20 22:09:49 -0300

    (chore): draft of utils module for how OCR will be used in this project .

---
+++ ocr_utils.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import easyocr # type: ignore
+
+reader = easyocr.Reader(['en'])
+
+def get_text(img):
+    # detail=0 will make readtext return a list of elements found in the image
+    return reader.readtext(img, detail=0)
+
+
