f65cb8fd85920b0cf7aed48ca79548319e86efb3
kelvesc <kelves_16@hotmail.com>
2024-09-08 20:19:41 -0300

    (chore): changed imports

--- main.py
+++ main.py
@@ -3,13 +3,14 @@
 
 import os
 import cv2
+import statistics
 import numpy as np
 
 from argparse import ArgumentParser, RawTextHelpFormatter
-import config
+from torchmetrics.text import CharErrorRate
+
 import image_utils
-# from ocr_utils import readtext
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from data_utils import load_csv_data
 
 __authors__ = "Kelves Costa, Paloma Raissa, Rubson Lima"
 __email__ = "kelves.nunes@ufrpe.br, palomaraissa10@gmail.com, limarbson7@gmail.com"
@@ -28,17 +29,9 @@ def __get_args():
 
     return parsed_args
 
-def validate(y_test, y_pred):
-    accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred, pos_label=3)
-    recall = recall_score(y_test, y_pred, pos_label=3)
-    f1 = f1_score(y_test, y_pred, pos_label=3)
-
-    print(f"""acc: {accuracy}
-          precision? {precision}
-          recall: {recall}
-          f1? {f1}""")
-
+def validate(y_read, y_target):
+    cer = CharErrorRate()
+    return cer(y_read, y_target)
 
 def main():
     args = __get_args()
@@ -47,31 +40,47 @@ def main():
     output_dir = args.output_dir
 
     if args.use_ocr:
-        labels = np.array([])
+        from ocr_utils import readtext
+        labels = []
 
     for filename in os.listdir(input_dir):
-        print(f"Processando imagem: {filename}")
         image_path = os.path.join(input_dir, filename)
-
         img = cv2.imread(image_path)
-
         if img is None:
             pass
+        
+        h = w = 100
+        img = cv2.resize(img, (h, w))
 
+        img = image_utils.high_pass(img)
+        img = image_utils.kmeans_segmentation(img)
         img = image_utils.preprocessing(img)
-        img = image_utils.log_img(img)
         img = image_utils.normalize(img)
-        img = image_utils.binarize_niblack(img)
-        img = image_utils.gray_segmentation(img)
+        
+        # img = image_utils.bhpf(img)
 
 
         if args.use_ocr:
-            text = readtext(img, reader='easyocr')
-            labels = np.append(labels, text)
+            text_from_img = readtext(img, reader='easyocr')
+            text = '' if text_from_img == [] else min(text_from_img, key=len)
+            labels.append(text)
 
-        if args.save_output:# and img is not None: # and not args.output_dir
+        if args.save_output:
             output_path = os.path.join(output_dir, filename)
             cv2.imwrite(output_path, img)
+
+    if args.use_ocr:
+        all_data = load_csv_data()
+        all_labels = all_data['label']
+
+        err = []
+        for idx in range(len(labels)):
+            text_read = labels[idx]
+            text_target = all_labels[idx]
+            err.append(validate(text_read, text_target))
+            # print(f'label: {text_read}\t target: {text_target}\t error: {validate(text_read, text_target)}')
+
+        print(f'The avarage error is {statistics.fmean(err)}')
 
 if __name__ == '__main__':
     main()
